2a9da8c130d48f685543b17b127e18d320c76460
vaibhavpohankar <vaibhavpohankar30@gmail.com>
2026-01-06 15:26:46 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Welcome vaibhv's Pipeline";
+    private static final String MESSAGE = "Welcome vaibhav's jenkin Pipeline";
 
     public App() {}
 
