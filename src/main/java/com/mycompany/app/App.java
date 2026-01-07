a1552a94b6272d9d4dec47686aafc678aff5dba3
vaibhavpohankar <vaibhavpohankar30@gmail.com>
2026-01-07 11:45:12 +0000

    app file updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Welcome to vaibhav's jenkin Pipeline";
+    private static final String MESSAGE = "Welcome to vaibhav's jenkin Pipeline with Docker";
 
     public App() {}
 
